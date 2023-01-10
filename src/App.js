e445c493cbb9ba0eda1916171d501d0b67e63abb
richardschmidtDEV <RichardSchmidtDEV@outlook.com>
2023-01-10 15:21:50 -0500

    changes made from theia lab environment

--- src/App.js
+++ src/App.js
@@ -2,12 +2,23 @@ import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 // Add code to import the components
 import { AppProvider } from './context/AppContext';
+import AllocationForm from './components/AllocationForm';
+import Budget from './components/Budget';
+import ExpenseItem from './components/ExpenseItem';
+import ExpenseList from './components/ExpenseList';
+import ExpenseTotal from './components/ExpenseTotal';
+import Remaining from './components/Remaining';
 const App = () => {
     return (
         <AppProvider>
             <div className='container'>
-                {/* Add the code here to add the components          */}
-            </div>
+                <AllocationForm />
+                <Budget />
+                <ExpenseItem />
+                <ExpenseList />
+                <ExpenseTotal />
+                <Remaining />
+                </div>
         </AppProvider>
     );
 };
